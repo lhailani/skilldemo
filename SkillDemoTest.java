78dee3f235d19e7798f0a872f3f54e41e9ac2de4
lhailani <97707886+lhailani@users.noreply.github.com>
2022-02-04 05:10:12 -0800

    passed test

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void subtraction() {
+        assertEquals(2, SkillDemo.subtract(4, 2)); //fixed test
+    }
+}
